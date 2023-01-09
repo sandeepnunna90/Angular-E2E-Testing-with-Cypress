c8e784ab0d5ed70b42444b642fa70998ddc8bb79
Sandeep Nunna <snunna@centrak.com>
2023-01-08 22:54:43 -0500

    Creating our first test

---
+++ cypress/integration/adventure.spec.ts
@@ -0,0 +1,10 @@
+describe('Adventure', () => {
+  it('should visit CarvedRock homepage', () => {
+    cy.visit('/');
+  });
+
+  it('should open the Breithorn adventure', () => {
+    cy.get('a[href="/adventure/1"]').click();
+    cy.get('#title').should('have.text', 'Breithorn, Pennine Alps');
+  });
+});
