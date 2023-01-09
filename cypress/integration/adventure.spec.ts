839bb2cfe48af8c20b5db33dc312e0cf41365b0d
Sandeep Nunna <snunna@centrak.com>
2023-01-08 23:31:22 -0500

    Interacting with DOM ELements

--- cypress/integration/adventure.spec.ts
+++ cypress/integration/adventure.spec.ts
@@ -7,4 +7,32 @@ describe('Adventure', () => {
     cy.get('a[href="/adventure/1"]').click();
     cy.get('#title').should('have.text', 'Breithorn, Pennine Alps');
   });
+
+  it('should post a comment', () => {
+    cy.contains('Reset Comments').click();
+    cy.contains('Add Comment').click();
+
+    cy.get('#name').type('Josh');
+    cy.get('#comment-text').type('What a great adventure!');
+    cy.get('#add-comment-button').click();
+
+    cy.get('div[data-test-automation="user-comments"] blockquote:last-child p')
+        .should('have.text', 'What a great adventure!');
+    cy.get('div[data-test-automation="user-comments"] blockquote:last-child footer')
+        .should('have.text', 'Josh');
+
+    cy.get('div[data-test-automation="user-comments"] blockquote:last-child').then($el => {
+      cy.wrap($el).find('p').should('have.text', 'What a great adventure!');
+      cy.wrap($el).find('footer').should('have.text', 'Josh');
+    });
+  });
+
+  it('should not post a comment if the comment text is missing', () => {
+    cy.contains('Add Comment').click();
+
+    cy.get('#name').type('Josh');
+    cy.get('#add-comment-button').click();
+    cy.get('div[data-test-automation="comment-text"] .text-danger')
+      .should('have.text', 'Comment is required.');
+  });
 });
